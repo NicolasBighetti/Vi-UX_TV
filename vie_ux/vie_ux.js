25b6ce4b79d8b793acb477ca02c74bc89baab140
Unknown <nicobighetti@gmail.com>
2018-01-29 11:28:34 +0100

    should be working (voice recognition needs to be tested)

--- vie_ux/vie_ux.js
+++ vie_ux/vie_ux.js
@@ -1,17 +1,17 @@
 function menuClick() {
-	document.getElementById('menu').style.display = 'none';
-	document.getElementById('record').style.display = 'inline';
+	document.getElementById('gridDisplay').style.display = 'none';
+	document.getElementById('contextualPopup').style.display = 'inline';
 	document.getElementById('envoye').style.display = 'none';
 };
 
 function recordClick() {
-	document.getElementById('menu').style.display = 'none';
-	document.getElementById('record').style.display = 'none';
+	document.getElementById('gridDisplay').style.display = 'none';
+	document.getElementById('contextualPopup').style.display = 'none';
 	document.getElementById('envoye').style.display = 'inline';
 };
 
 function envoyeClick() {
-	document.getElementById('menu').style.display = 'inline';
-	document.getElementById('record').style.display = 'none';
+	document.getElementById('gridDisplay').style.display = 'inline';
+	document.getElementById('contextualPopup').style.display = 'none';
 	document.getElementById('envoye').style.display = 'none';
 };
